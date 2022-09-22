e19fd94794695904eca7aac7a15b54f592bd45bc
Themba Real Lukhele <realthemba@gmail.com>
2022-09-22 18:46:34 +0200

    setting scroll and keyboard shortcuts

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,29 @@
   Email : realthemba@gmail.com
   Organisation : Siyamuza 
 */
+(function($) {
+
+	// Settings.
+		var settings = {
+
+			// Keyboard shortcuts.
+				keyboardShortcuts: {
+
+					// If true, enables scrolling via keyboard shortcuts.
+						enabled: true,
+
+					// Sets the distance to scroll when using the left/right arrow keys.
+						distance: 50
+
+				},
+
+			// Scroll wheel.
+				scrollWheel: {
+
+					// If true, enables scrolling via the scroll wheel.
+						enabled: true,
+
+					// Sets the scroll wheel factor. (Ideally) a value between 0 and 1 (lower = slower scroll, higher = faster scroll).
+						factor: 1
+
+				},
